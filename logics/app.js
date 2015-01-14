e945cb6f6012ebc01121021dede352ad5c792493
jamesman11 <huaxianm@gmail.com>
2015-01-14 01:13:05 -0800

    setting basic structure for the game

--- logics/app.js
+++ logics/app.js
@@ -1,34 +1,179 @@
-var App = App || {};
-
+var App = App || {},
+    $bCanvas = $('#board-canvas')[0],
+    $tCanvas = $('#tile-canvas')[0],
+    $nCanvas = $('#next-canvas')[0],
+    time = 0,
+    speed = { start: 0.6, decrement: 0.005, min: 0.1 },
+    last = now =  new Date().getTime();
 App.init = function(){
+    this.btx = $bCanvas.getContext('2d');
+    this.ttx = $tCanvas.getContext('2d');
+    this.ntx = $nCanvas.getContext('2d');
+    this.width = $bCanvas.width;
+    this.height = $bCanvas.height;
+    this.curRow = this.CONFIG.DEFAULT_STARTING_POINT['row'];
+    this.curCol = this.CONFIG.DEFAULT_STARTING_POINT['col'];
+    this.pieces = this.CONFIG.TILES_HELPER;
+    this.colCount = this.width / this.CONFIG.WIDTH_PER_GRID;
+    this.rowCount = this.height / this.CONFIG.WIDTH_PER_GRID;
+    this.rows = 0;
+    this.actions = [];
+    this.newTile();
     this.drawBoardLines();
+    this.createBoard();
+    this.bindKeyEvent();
+    this.drawNext();
+    //this.frame();
+};
+App.clearRows = function(){
+    this.rows = 0;
+};
+App.curSpeed = function(){
+    return Math.max(speed.min, speed.start - (speed.decrement * this.rows))
+};
+App.frame = function(){
+    var now = new Date().getTime();
+    App.update(Math.min(1, (now - last) / 1000.00));
+    App.drawTiles();
+    last = now;
+    requestAnimationFrame(App.frame);
+};
+App.update = function(timePass){
+    time = time + timePass;
+    var curSpeed = this.curSpeed();
+    this.handle(this.actions.shift());
+    if (time > curSpeed){
+        time = time - curSpeed;
+        this.drop();
+    }
+};
+App.handle = function(action){
+    switch(action) {
+        case this.CONFIG.DIR.LEFT:  this.move(this.CONFIG.DIR.LEFT);  break;
+        case this.CONFIG.DIR.RIGHT: this.move(this.CONFIG.DIR.RIGHT); break;
+        case this.CONFIG.DIR.UP:    this.rotate();        break;
+        case this.CONFIG.DIR.DOWN:  this.drop();          break;
+    }
 };
 App.drawBoardLines = function(){
-    var colCount = this.width / this.CONFIG.WIDTH_PER_GRID;
-    var rowCount = this.height / this.CONFIG.WIDTH_PER_GRID;
-    this.context.lineWidth = 0.5;
-    this.context.strokeStyle = '#f0d7b4';
+    this.btx.lineWidth = 0.5;
+    this.btx.strokeStyle = this.CONFIG.BOARD_LINE_COLOR;
     // draw vertical
-    for(var i = 0; i < colCount; i++){
+    for(var i = 0; i < this.colCount; i++){
         var curPos = (i + 1)*this.CONFIG.WIDTH_PER_GRID;
-        this.context.beginPath();
-        this.context.moveTo(curPos, 0);
-        this.context.lineTo(curPos, this.height);
-        this.context.stroke();
+        this.btx.beginPath();
+        this.btx.moveTo(curPos, 0);
+        this.btx.lineTo(curPos, this.height);
+        this.btx.stroke();
     }
     // draw horizontal
-    for (var i = 0; i < rowCount; i++){
+    for (var i = 0; i < this.rowCount; i++){
         var curPos = (i + 1)*this.CONFIG.WIDTH_PER_GRID;
-        this.context.beginPath();
-        this.context.moveTo(0, curPos);
-        this.context.lineTo(this.width, curPos);
-        this.context.stroke();
+        this.btx.beginPath();
+        this.btx.moveTo(0, curPos);
+        this.btx.lineTo(this.width, curPos);
+        this.btx.stroke();
+    }
+};
+App.bindKeyEvent = function(){
+    var handled = false;
+    $('body').keydown(function(ev){
+        switch (ev.keyCode) {
+            case App.CONFIG.KEY.LEFT:
+                handled = true;
+                App.actions.push(App.CONFIG.DIR.LEFT);
+                break;
+            case App.CONFIG.KEY.RIGHT:
+                handled = true;
+                App.actions.push(App.CONFIG.DIR.RIGHT);
+                break;
+            case App.CONFIG.KEY.UP:
+                handled = true;
+                App.actions.push(App.CONFIG.DIR.UP);
+                break;
+            case App.CONFIG.KEY.DOWN:
+                handled = true;
+                App.actions.push(App.CONFIG.DIR.DOWN);
+                break;
+        }
+        if (handled) ev.preventDefault();
+    });
+};
+App.move = function(direction){
+    switch (direction){
+        case this.CONFIG.DIR.LEFT:
+            this.curCol--;
+            break;
+        case this.CONFIG.DIR.RIGHT:
+            this.curCol++;
+            break;
+
+    }
+};
+App.rotate = function(){
+    this.curMovIndex = this.curMovIndex == 3 ? 0 : this.curMovIndex + 1;
+    this.drawTiles();
+};
+App.drop = function(){
+    this.curRow++;
+    this.drawTiles();
+};
+App.newTile = function(){
+    this.curTile = this.ramdomPiece();
+    this.curMovIndex = 0;
+}
+App.drawTiles = function(){
+    var bitCheck = 0x0001;
+    var row = 4, col = 4;
+    var curTileConfig = this.CONFIG.TILES[this.curTile];
+    this.ttx.clearRect(0, 0, this.width, this.height);
+    for(var bit = curTileConfig['blocks'][this.curMovIndex]; bit > 0; bit = bit >> 1){
+        if(parseInt(bit & bitCheck) == 1){
+            var x = (this.curCol + col - 1) * this.CONFIG.WIDTH_PER_GRID;
+            var y = (this.curRow + row - 1) * this.CONFIG.WIDTH_PER_GRID;
+            this.drawTile(x, y, this.CONFIG.WIDTH_PER_GRID, curTileConfig['color'], this.ttx);
+        }
+        if (--col === 0){
+            col = 4;
+            row--;
+        }
+    }
+};
+App.drawNext = function(){
+    var bitCheck = 0x0001;
+    var row = 4, col = 4;
+    var curTileConfig = this.CONFIG.TILES[this.curTile];
+    this.ntx.clearRect(0, 0, this.ntx.width,this.ntx.height);
+    for(var bit = curTileConfig['preview']; bit > 0; bit = bit >> 1){
+        if(parseInt(bit & bitCheck) == 1){
+            var x = (col - 1) * (this.CONFIG.PREVIEW_TILE_WIDTH + 1);
+            var y = (row - 1) * (this.CONFIG.PREVIEW_TILE_WIDTH + 1);
+            this.drawTile(x, y, this.CONFIG.PREVIEW_TILE_WIDTH, curTileConfig['color'], this.ntx);
+        }
+        if (--col === 0){
+            col = 4;
+            row--;
+        }
+    }
+};
+App.drawTile = function(x, y, width, color, context){
+    context.fillStyle = color;
+    context.fillRect(x,y,width,width);
+};
+App.ramdomPiece = function(){
+    if (this.pieces.length == 0) this.pieces = this.CONFIG.TILES_HELPER;
+    var index = Math.floor(Math.random() * this.pieces.length);
+    return this.pieces.splice(index, 1)[0];
+};
+App.createBoard = function(){
+    App.board = [];
+    for(var row = 0;row < this.rowCount; row++){
+        App.board[row] = new Array();
+        for (var col = 0; col < this.colCount; col++){
+            App.board[row].push(0);
+        }
     }
 };
 $(document).ready(function(){
-    var $canvas = $('#board-canvas')[0];
-    App.context = $canvas.getContext('2d');
-    App.width = $canvas.width;
-    App.height = $canvas.height;
     App.init();
-})
+});
--- logics/config.js
+++ logics/config.js
@@ -1,4 +1,42 @@
 var App = App || {};
 App.CONFIG = {
-    WIDTH_PER_GRID : 35
+    WIDTH_PER_GRID : 35,
+    PREVIEW_TILE_WIDTH: 25,
+    DEFAULT_STARTING_POINT: { row: 0, col: 3},
+    BOARD_LINE_COLOR: '#f0d7b4',
+    SPEED: { start: 0.6, decrement: 0.005, min: 0.1 },
+    /*
+       all tiles are transformed to a hexadecimal number, each digit indicates whether a tile exists at a specific position
+       blocks: each element represents a rotation of the piece (0, 90, 180, 270)
+              each element is a 16 bit integer where the 16 bits represent
+              a 4x4 set of blocks, e.g. j.blocks[0] = 0x44C0
+
+                  0100 = 0x4 << 3 = 0x4000
+                  0100 = 0x4 << 2 = 0x0400
+                  1100 = 0xC << 1 = 0x00C0
+                  0000 = 0x0 << 0 = 0x0000
+                                    ------
+                                    0x44C0
+
+       PREW_MAX_HORI and PREW_MAX_VERTI are the count of how many tiles vertically and horizontally
+       They are used for calculating the offset, so we can center them in the preview box
+     */
+    TILES : {
+        "I" : { blocks: [0x0F00, 0x2222, 0x0F00, 0x2222], color: '#6CA766', preview: 0x0F00},
+        "J" : { blocks: [0x2260, 0x0470, 0x6440, 0x7100], color: '#73adc1', preview: 0x0470},
+        "O" : { blocks: [0x6600, 0x6600, 0x6600, 0x6600], color: '#815285', preview: 0x0660},
+        "S" : { blocks: [0x6C00, 0x4620, 0x6C00, 0x4620], color: '#B26D86', preview: 0x06C0},
+        "T" : { blocks: [0xE400, 0x2620, 0x04E0, 0x8C80], color: '#CC9D7D', preview: 0x0E40},
+        "L" : { blocks: [0x4460, 0x0740, 0x6220, 0x02E0], color: '#FF6C36', preview: 0x02E0},
+        "Z" : { blocks: [0xC600, 0x2640, 0xC600, 0x2640], color: '#FFE460', preview: 0X0C60}
+    },
+    TILES_HELPER : ["I","I","I","I",
+                    "J","J","J","J",
+                    "O","O","O","O",
+                    "S","S","S","S",
+                    "T","T","T","T",
+                    "L","L","L","L",
+                    "Z","Z","Z","Z"],
+    KEY : { ESC: 27, SPACE: 32, LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40 },
+    DIR : { UP: 0, RIGHT: 1, DOWN: 2, LEFT: 3}
 }
